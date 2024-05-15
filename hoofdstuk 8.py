b0d35c133aed862d629de974a67911e028b74baf
RowanPl <Rowanplooij@hotmail.com>
2024-05-15 09:19:31 +0200

    initialize

---
+++ hoofdstuk 8.py
@@ -0,0 +1,30 @@
+# Opgave 1:
+# Gegeven een woordenboek van Internet toplevel domeinen:
+
+# tlds = {'Nederland':'nl', 'Verenigde Staten':'us', 'Duitsland':'de'}
+
+# Voer de volgende opdrachten uit en toon de resultaten:
+
+#     Controleer of het woordenboek de sleutel ‘Duitsland’ bevat.
+#     Controleer of het woordenboek de sleutel ‘Frankrijk’ bevat.
+#     Itereer door de sleutel-waarde paren en toon ze in 2-kolommen.
+#     Voeg het sleutel-waarde paar ‘Zweden’ : ‘sw’ toe.
+#     Wijzig de waarde van de sleutel ‘Zweden’ in ‘se’.
+#     Gebruik een dictionary comprehension om sleutels en waarden te verwisselen.
+#     Uitgaande van het resultaat van f) gebruik een dictionary comprehension om alle landnamen te converteren naar hoofdletters.
+
+# Opgave 2:
+# Maak een woordenboek van vrienden met hun voornaam als sleutel en hun leeftijd als waarde (we nemen even aan dat alle voornamen verschillend zijn).
+
+# Voer de volgende opdrachten uit en toon de resultaten:
+#     Druk naam en leeftijd af van al je vrienden, alfabetisch gesorteerd op naam.
+#     Druk de naam af van al je vrienden die 30 jaar of ouder zijn.
+#     Verhoog alle leeftijden in het woordenboek met 1.
+#     Verwijder alle vrienden waarvan de naam begint met de letter ‘B’
+
+
+
+# Opgave 3:
+# Breid het programma waarin we de zoeksnelheid in een lijst en woordenboek vergelijken uit
+# zodat je 1000 random gehele getallen tussen 0 en 1 miljoen genereert en die probeert op te zoeken in de lijst
+# en het woordenboek. Wat is het resultaat?
---
+++ hoofdstuk 9.py
@@ -0,0 +1,28 @@
+# Opgave 1:
+# Gegeven de verzameling {3, 44, 17, 23, 58, 9, 36}
+
+# Voer de volgende opdrachten uit:
+
+#     Voeg de waarde 27 aan de verzameling toe.
+#     Verwijder de waarde 23 uit de verzameling’.
+#     Druk alle waarden in de verzameling tussen 20 en 50 af.
+
+
+# Opgave 2:
+# Gegeven de verzamelingen {11, 22, 33} en {5, 11, 16, 22}
+# Gebruik wiskundige verzamelingsoperatoren om de volgende verzamelingen te creëren:
+
+#     {33}
+#     {5, 16, 33}
+#     {5, 11, 16, 22, 33}
+#     {11, 22}
+
+
+# Opgave 3:
+# Maak 2 verzamelingen met kleuren aan en controleer dat het symmetrisch verschil van die verzamelingen gelijk is aan de
+# vereniging van de verschillen tussen beide verzamelingen. In een wiskundige formule: : A B = (A ∖ B) (B ∖ A)
+
+
+# Opgave 4:
+# Maak nogmaals 2 verzamelingen met kleuren aan en controleer dat het symmetrisch verschil van die verzamelingen gelijk
+# is aan het verschil tussen de vereniging en de doorsnede van beide verzamelingen. In een wiskundige formule: : A B = (A B) ∖ (A B)
