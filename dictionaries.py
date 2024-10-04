96b4369875d3e342476ee8e3adfb08dc2e6a9396
Danielooo <dvanraffe@gmail.com>
2024-10-04 20:59:22 +0200

    finished dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -3,7 +3,7 @@
 #
 # Maak een dictionary genaamd 'fruit' met daarin de sleutels 'appel' en 'banaan'.
 # Geef 'appel' de value 'rood' en 'banaan' de value 'geel'.
-# Print vervolgens de kleur van de banaan door de key 'banaan' te gebruiken.
+# Print vervolgens de kleur van de banaan door de naam 'banaan' te gebruiken.
 #
 # Verwachte uitkomst:  geel
 # ==========================================
@@ -15,205 +15,161 @@ fruit = {'appel': 'rood', 'banaan': 'geel'}
 print(fruit['banaan'])  # Het resultaat is: geel
 
 
+### WAARDE AANROEPEN
 
 # ==========================================
 # Opdracht 1:
+# Gegeven is de dictionary 'boodschappen' met daarin de keys 'Appels' en 'Brood'.
 # Maak de volgende deelopdrachten. Print de uitkomst van elke opdracht om te zien of je het juiste resultaat krijgt.
-
+#
 # Opdracht 1a:
-# Maak een woordenboek met daarin de domeinnamen van de landen Nederland, Verenigde Staten en Duitsland.
-# De waarden zijn respectievelijk de landcodes 'nl', 'us' en 'de'.
+# Roep de waarde aan die hoort bij de naam 'Brood'
+# Verwachte uitkomst: 2
 #
 # Opdracht 1b:
-# Controleer of de woordenboek het land 'Nederland' bevat. Zo ja, print 'True', zo nee, print 'False'.
-# Doe dit ook voor de landen 'Verenigde Staten' en 'India'.
-#
-# Verwachte uitkomst: True, True, False
-#
-# Opdracht 1c:
-# Itereer door de key-waarde paren en toon per regel de key en de waarde.
-# Gebruik hierbij een for-loop en de items() methode.
-#
-# Verwachte uitkomst:
-#
-#   Nederland nl
-#   Verenigde Staten us
-#   Duitsland de
-#
-# Opdracht 1d:
-# Voeg het key-waarde paar ‘Zweden’ : ‘sw’ toe aan het woordenboek 'domeinen'.
-# Wijzig de waarde van de key ‘Zweden’ in ‘se’.
-# Het key waarde paar ‘Zweden’ : ‘se’ moet nu in het woordenboek staan.
-# Print het woordenboek na deze wijzigingen.
-#
-# Verwachte uitkomst: {'NL': 'Nederland', 'US': 'Verenigde Staten', 'DE': 'Duitsland', 'SE': 'Zweden'}
-
-# Opdracht 1e:
-# Gebruik een dictionary comprehension om de keys en waarden van het woordenboek te verwisselen.
-# Gebruik vervolgens een dictionary comprehension om alle landnamen te converteren naar hoofdletters.
-# Print het woordenboek na deze wijzigingen.
-#
-# Verwachte uitkomst: {'NL': 'Nederland', 'US': 'Verenigde Staten', 'DE': 'Duitsland', 'SE': 'Zweden'}
+# Gebruik de get() methode op de waarde 'Appels' en 'Bananen'. Zorg dat als de naam niet bestaat, de tekst 'Niet gevonden' wordt geprint.
 # ==========================================
 
-# Opdracht 1a: woordenboek met domeinnamen en landcodes
-domeinen = {'Nederland': 'nl', 'Verenigde Staten': 'us', 'Duitsland': 'de'}
+boodschappen = {'Appels': 6, 'Brood': 2}
 
-# Opdracht 1b: controleer of het woordenboek de landen bevat
-print(domeinen.get('Nederland') != None)  # Het resultaat is: True
-print(domeinen.get('Verenigde Staten') != None)  # Het resultaat is: True
-print(domeinen.get('India') != None)  # Het resultaat is: False
+# Opdracht 1a:
+print(boodschappen['Brood'])  # Het resultaat is: 2
 
-# Opdracht 1c: itereer door de key-waarde paren en toon per regel de key en de waarde
-for key, value in domeinen.items():
-    print(key, value)
-
-# Verwachte uitkomst:
-#   Nederland nl
-#   Verenigde Staten us
-#   Duitsland de
-
-# Opdracht 1d: voeg een key-waarde paar toe en wijzig de waarde van de key
-#     Voeg het key-waarde paar ‘Zweden’ : ‘sw’ toe.
-domeinen['Zweden'] = 'sw'
-#     Wijzig de waarde van de key ‘Zweden’ in ‘se’.
-domeinen['Zweden'] = 'se'
-
-
-# Opdracht 1e:
-# in deze dictionary comprehension worden de keys en waarden van het woordenboek verwisseld
-domeinen = {value: key for key, value in domeinen.items()}  # Uitkomst: {'nl': 'Nederland', 'us': 'Verenigde Staten', 'de': 'Duitsland', 'se': 'Zweden'}
-# landnamen worden omgezet naar hoofdletters
-domeinen = {key: value.upper() for key, value in domeinen.items()}  # Uitkomst: {'NL': 'Nederland', 'US': 'Verenigde Staten', 'DE': 'Duitsland', 'SE': 'Zweden'}
-
-print(domeinen)
+# Opdracht 1b:
+print(boodschappen.get('Appels', 'Niet gevonden'))  # Het resultaat is: 6
+print(boodschappen.get('Bananen', 'Niet gevonden'))  # Het resultaat is: Niet gevonden
 
 
 
-
+### WOORDENBOEKEN VERGELIJKEN
 
 # ==========================================
 # Opdracht 2:
+# Gegeven zijn vier boodschappenlijstjes als dictionary. Je kan een if statement gebruiken en de == operator om te vergelijken.
+#
+# Opdracht 2a:
+# Vergelijk de boodschappenlijstjes van Marie en Raj. Als de lijstjes gelijk zijn, print 'De boodschappenlijstjes zijn gelijk.'.
+#
+# Opdracht 2b:
+# Vergelijk de boodschappenlijstjes (Marie, Raj en Jan). Als de lijstjes gelijk zijn, print 'De boodschappenlijstjes zijn gelijk.'.
+# Waarom worden deze lijstjes wel/niet als gelijk beschouwd?
+#
+# Opdracht 2c:
+# Vergelijk nu op dezelfde manier alle lijstjes. Zijn ze gelijk? Waarom wel/niet?
+
+boodschappenlijst_marie = {'Brood': 1, 'Appels': 6}
+boodschappenlijst_raj = {'Brood': 1, 'Appels': 6}
+boodschappenlijst_jan = {'Appels': 6, 'Brood': 1}
+boodschappenlijst_karel = {'Appels': 6, 'Brood': 25}
+
+# Opdracht 2a:
+if boodschappenlijst_marie == boodschappenlijst_raj:
+    print("De boodschappenlijstjes zijn gelijk.")
+else:
+    print("De boodschappenlijstjes zijn niet gelijk.")
+# Uitkomst: De boodschappenlijstjes zijn gelijk.
+
+# Opdraht 2b:
+if boodschappenlijst_marie == boodschappenlijst_raj == boodschappenlijst_jan:
+    print("De boodschappenlijstjes zijn gelijk.")
+else:
+    print("De boodschappenlijstjes zijn niet gelijk.")
+# Uitkomst: De boodschappenlijstjes zijn gelijk.
+# De dictionaries worden als gelijk beschouwd omdat de keys en values van de dictionaries overeenkomen.
+
+# Opdracht 2c:
+if boodschappenlijst_jan == boodschappenlijst_marie == boodschappenlijst_raj == boodschappenlijst_karel:
+    print("De boodschappenlijstjes zijn gelijk.")
+else:
+    print("De boodschappenlijstjes zijn niet gelijk.")
+# Uitkomst: De boodschappenlijstjes zijn niet gelijk.
+# De dictionaries worden niet als gelijk beschouwd omdat de values van de keys niet overeenkomen.
+
+
+### WOORDENBOEK AANPASSEN
+
+# ==========================================
+# Opdracht 2:
+# De voorraad van meubelwinkel 'Op Eigen Houtje' is als volgt:
+# Gegeven is de dictionary 'meubels' met daarin de keys 'banken' en 'stoelen'.
 # Maak de volgende deelopdrachten. Print de uitkomst van elke opdracht om te zien of je het juiste resultaat krijgt.
 #
 # Opdracht 2a:
-# Maak een woordenboek van de volgende vriendengroep met hun voornaam als key en hun leeftijd als waarde.
-# Kees is 25, Amber is 30, Bart is 28, Johan is 27 en Eva is 26.
-#
+# Er worden 6 banken verkocht. Pas de waarde van de naam 'banken' aan en print de aangepaste dictionary
+# Verwachte uitkomst: {'banken': 4, 'stoelen': 20}
+
 # Opdracht 2b:
-# Druk naam en leeftijd af van al je vrienden, alfabetisch gesorteerd op naam.
-#
-# Verwachte uitkomst:  Amber 30, Bart 28, Eva 26, Johan 27, Kees 25
-#
+# Er komen klachten dat klanten door eerder gekochte stoelen zijn gezakt. De winkel besluit om de stoelen uit het assortiment te halen.
+# Verwijder de naam 'stoelen' (en daarmee ook de bijbehorende value) uit de dictionary en print de aangepaste dictionary.
+# Verwachte uitkomst: {'banken': 4}
+
 # Opdracht 2c:
-# Druk de naam af van al je vrienden die 30 jaar of ouder zijn.
-#
-# Verwachte uitkomst: Amber
-#
-# Opdracht 2d:
-# Verhoog alle leeftijden in het woordenboek met 1.
-#
-# Verwachte uitkomst: {'Kees': 26, 'Amber': 31, 'Bart': 29, 'Johan': 28, 'Eva': 27}
-#
-# Opdracht 2e:
-# Verwijder alle vrienden waarvan de naam begint met de letter ‘B’.
-# Voor het verwijderen van items kun je het keyword 'del' gebruiken.
-#
-# Verwachte uitkomst: {'Kees': 26, 'Amber': 31, 'Johan': 28, 'Eva': 27}
-
+# De winkel gaat tafels verkopen en koopt er gelijk 15 in. Voeg de naam 'tafels' toe met een waarde van 15 en print de aangepaste dictionary.
+# Verwachte uitkomst: {'banken': 4, 'tafels': 15}
 # ==========================================
-
-friends = {'Kees': 25, 'Amber': 30, 'Bart': 28, 'Johan': 27, 'Eva': 26}
+meubels = {'banken': 10, 'stoelen': 20}
 
 # Opdracht 2a:
-#     De for loop gaat alle keys van het woordenboek langs en sorteert deze op alfabetische volgorde.
-for key in sorted(friends.keys()):
-    print(key, friends[key])
+meubels['banken'] -= 6
+print(meubels)  # Het resultaat is: {'banken': 4, 'stoelen': 20}
 
 # Opdracht 2b:
-#     De for loop gaat alle keys en values van het woordenboek langs en print de keys van de vrienden die 30 jaar of ouder zijn.
-#     .items() geeft een lijst van tuples terug met de key en value van het woordenboek.
-for key, value in friends.items():
-    if value >= 30:
-        print(key)
-#     friends[key] verwijst naar de value van de key in het woordenboek. += verhoogt de huidige waarde met de waarde die na het += teken staat.
-for key in friends.keys():
-    friends[key] += 1
-#     De for loop gaat alle keys van het woordenboek langs . .startswith() geeft True terug als de string begint met de meegegeven waarde.
-for key in list(friends.keys()):
-    if key.startswith('B'):
-        del friends[key]
+del meubels['stoelen']
+print(meubels)  # Het resultaat is: {'banken': 4}
 
-print(friends)
+# Opdracht 2c:
+meubels['tafels'] = 15
+print(meubels)  # Het resultaat is: {'banken': 4, 'tafels': 15}
 
 
-
-
+### LOOPEN DOOR WOORDENBOEK
 
 # ==========================================
-# Opgave 3:
+# Opdracht 3:
+# Docent Erik heeft proefwerken van studenten nagekeken en de cijfers in een dictionary opgeslagen.
+# Erik komt erachter dat hij een fout heeft gemaakt en moet de cijfers met een factor 1,5 vermenigvuldigen.
+# Met als enige uitzondering dat een cijfer nooit hoger mag zijn dan een 10.
+# Print de uitkomst om te zien of je het juiste resultaat krijgt.
 
-# We gaan kijken wat we sneller kunnen doorzoeken: Een lijst of een woordenboek.
-# De libraries 'random' en 'time' hebben we nodig. Die mag je alvast importeren.
-# Maak hiervoor de volgende deelopdrachten. Print de uitkomst van elke opdracht om te zien of je het juiste resultaat krijgt.
-#
-# Opdracht 3a:
-# Genereer een lijst met 1000 random getallen tussen 0 en 1 miljoen.
-# Genereer dan een woordenboek die dezelfde random getallen heeft als de lijst. De keys zijn de random getallen en alle waarden zijn None.
-# Voorbeeld list: [63459, 27, ...enz, ...enz]
-# Voorbeeld dictionary: {63459: None, 27: None, ...enz, ...enz}
-#
-# Opdracht 3b:
-# Maak een variabele counter_list aan en zet deze op 0
-# Maak een for loop die 1000000 keer loopt.
-# In de for loop controleer je of de loop variabele i in de random_list voorkomt. Zo ja, verhoog dan de counter_list met 1.
-# time.time() geeft de huidige tijd in seconden terug. Reken met behulp van deze methode uit hoe lang de loop duurde.
-# Print de counter_list en de tijd die de loop duurde.
-#
-# Doe hetzelfde voor het woordenboek.
+# Gegeven is de dictionary 'cijfers' met daarin de keys 'Jaap', 'Winnie', 'Jeroen' en 'Lana' en hun cijfers.
+# Verwachte uitkomst: {'Jaap': 4.5, 'Winnie': 6.0, 'Jeroen': 10, 'Lana': 10}
 # ==========================================
 
-# Let op! Normaal zet je de imports bovenaan je bestand, maar voor de duidelijkheid van de opdracht zetten we ze hier.
-import random
-import time
+cijfers = {'Jaap': 3, 'Winnie': 4, 'Jeroen': 9, 'Lana': 10}
 
-#   3a
-#   random.randint() genereert een random getal tussen de 2 meegegeven waarden (hier dus tussen de 0 en 1000000).
-#   We willen 1000 keer een random getal genereren. Dat doet de for loop.
-#   In een for loop moet je altijd een naam geven aan de loop variabele.
-#   In dit geval gebruiken we de loop variabele verder niet, het is dan conventie om deze als underscore '_' te schrijven.
-random_list = [random.randint(0, 1000000) for _ in range(1000)]  # uitkomst
+for naam, cijfer in cijfers.items():
+    if cijfer * 1.5 > 10:
+        cijfers[naam] = 10
+    else:
+        cijfers[naam] = cijfer * 1.5
 
-#   Vult de dictionary met dezelfde random getallen als keys en None als values.
-#   Voor de opdracht maakt het niet uit wat de waarden zijn.
-#   We loopen alleen over de keys heen dus de waarden worden niet gebruikt.
-random_dict = {i: None for i in random_list}
+print(cijfers)  # Het resultaat is: {'Jaap': 4.5, 'Winnie': 6.0, 'Jeroen': 10, 'Lana': 10}
 
 
-#  3b
-# time.time() geeft de huidige tijd in seconden terug.
-# Door voor en na de loop de tijd op te vragen, en tegen elkaar weg te strepen (end - start), weet je hoe lang de loop duurde.
-start = time.time()
-# counter_list gaat bijhouden hoe vaak een random getal in de lijst voorkomt.
-# We willen namelijk dubbelchecken of de loop wel 1000 random getallen vind
-counter_list = 0
-# De for loop loopt 1000000 keer.
-for i in range(1000000):
-    # Als de loop variabele i overeenkomt met een random getal in de random_list, dan verhogen we de counter_list met 1.
-    if i in random_list:
-        counter_list += 1
-# hier vragen we nogmaals de tijd op zodat we de eindtijd van de loop hebben.
-end = time.time()
-# Print de counter en de tijd die de loop duurde.
-print(f"List; Counter: {counter_list}  Time: {end - start}")
+### SORTEREN VAN WOORDENBOEK
 
+# ==========================================
+# Opdracht 4:
+# De eigenaar van dierentuin 'Het Zootje' heeft een lijst met dieren en hun aantallen.
+#
+# Opdracht 4a:
+# De eigenaar wil de dieren op alfabetische volgorde zien. Sorteer de dieren op naam en print de dictionary uit.
+# Verwachte uitkomst: {'Aap': 5, 'Giraffe': 2, 'Leeuw': 3, 'Olifant': 4, 'Zebra': 1}
+#
+# Opdracht 4b:
+# Nu wil de eigenaar in een oogopslag wat het hoogste aantal is en het laagste. Sorteer nu de aantallen van hoog naar laag en print de dictionary uit.
+# ==========================================
 
-# We doen hier hetzelfde maar dan voor het woordenboek. Hierna kunnen we zien of de list of het woordenboek sneller te doorzoeken is.
-start = time.time()
-counter_dict = 0
-for i in range(1000000):
-    if i in random_dict:
-        counter_dict += 1
-end = time.time()
-print(f"Dictionary; Counter: {counter_dict}  Time: {end - start}")
+dieren = {'Olifant': 5, 'Zebra': 8, 'Aap': 12, 'Giraffe': 3, 'Neushoorn': 7}
 
+# Opdracht 4a:
+for dier in sorted(dieren.keys()):
+    print(dier, end=' ')
+# Het resultaat is: Aap Giraffe Leeuw Olifant Zebra
+
+print()  # Print een lege regel omdat anders de volgende print statement op dezelfe regel komt
+
+# Opdracht 4b:
+for aantal in sorted(dieren.values()):
+    print(aantal, end=' ')
+# Het resultaat is: 3 5 7 8 12
