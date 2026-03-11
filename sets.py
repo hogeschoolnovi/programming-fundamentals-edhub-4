137ed1bd4bcebacbfd631423fbc0275c5f640a3c
MRensen <markrensen@hotmail.com>
2026-03-11 16:46:48 +0100

    antwoorden verwijderd

--- sets.py
+++ sets.py
@@ -30,15 +30,6 @@ print('getallen: ', getallen)  # Het resultaat is: {1, 2, 3, 4}
 
 verzameling = {3, 44, 17, 23, 58, 9, 36}
 
-#     Voegt de value 27 aan de verzameling toe.
-verzameling.add(27)
-#     Verwijdert de value 23 uit de verzameling.
-verzameling.remove(23)
-#     Loopt door alle values in 'verzameling' en drukt de values af die tussen 20 en 50 liggen.
-#     In de if wordt er gecheckt of de linker value kleiner is dan de rechter value. Dus is 20 minder dan 'i' EN is 'i' minder dan 50.
-for i in verzameling:
-    if 20 < i < 50:
-        print(i)
 
 
 
@@ -55,12 +46,6 @@ for i in verzameling:
 verzameling_kleuren_1 = {'red', 'blue', 'green'}
 verzameling_kleuren_2 = {'yellow', 'blue', 'green'}
 
-#   checkt of de kleuren in verzameling_kleuren_1 zitten maar niet in verzameling_kleuren_2
-print(verzameling_kleuren_1 - verzameling_kleuren_2)
-print(verzameling_kleuren_1.difference(verzameling_kleuren_2))
-#   checkt welke kleuren niet in beide sets zitten
-print(verzameling_kleuren_1 ^ verzameling_kleuren_2)
-print(verzameling_kleuren_1.symmetric_difference(verzameling_kleuren_2))
 
 
 
@@ -78,18 +63,6 @@ print(verzameling_kleuren_1.symmetric_difference(verzameling_kleuren_2))
 
 verzameling1 = {11, 22, 33}
 verzameling2 = {5, 11, 16, 22}
-
-#     Haalt de values uit verzameling1 die niet in verzameling2 zitten.
-print(verzameling1 - verzameling2)  # Het resultaat is: {33}
-
-#     Haalt de values uit verzameling2 die niet in verzameling1 zitten.
-print(verzameling2 - verzameling1)  # De getallen in de set zijn: 5, 16
-
-#     Voegt de values van verzameling1 en verzameling2 samen.
-print(verzameling1 | verzameling2)  # De getallen in de set zijn: 33, 5, 11, 16, 22
-
-#     Haalt de values uit verzameling1 en verzameling2 die in beide sets zitten.
-print(verzameling1 & verzameling2)  # De getallen in de set zijn: 11, 22
 
 
 
